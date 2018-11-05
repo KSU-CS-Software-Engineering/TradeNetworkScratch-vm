49bcfdd68cd29f1e8fc624c41096fb3920dadf22
Zach Martin <zmironman@gmail.com>
2018-11-05 13:39:38 -0600

    First commit with extension prototype

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -16,6 +16,7 @@ const Scratch3TranslateBlocks = require('../extensions/scratch3_translate');
 const Scratch3VideoSensingBlocks = require('../extensions/scratch3_video_sensing');
 const Scratch3Speech2TextBlocks = require('../extensions/scratch3_speech2text');
 const Scratch3Ev3Blocks = require('../extensions/scratch3_ev3');
+const ZachTestBlocks = require('../extensions/zach-test');
 
 const builtinExtensions = {
     pen: Scratch3PenBlocks,
@@ -26,7 +27,8 @@ const builtinExtensions = {
     translate: Scratch3TranslateBlocks,
     videoSensing: Scratch3VideoSensingBlocks,
     speech2text: Scratch3Speech2TextBlocks,
-    ev3: Scratch3Ev3Blocks
+    ev3: Scratch3Ev3Blocks,
+    test: ZachTestBlocks
 };
 
 /**
